ccfb9b632643ebe7e4c98897dc7c8e384a542452
Rajeev Kumar <96237505+Rajeevkumar69@users.noreply.github.com>
2026-01-13 17:09:27 +0530

    Uncomment user-data route for lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-     // {
-     //      path:'user-data',
-     //      loadComponent:() => import('./user-data/user-data.component').then(com => com.UserDataComponent)
-     // }
+     {
+          path:'',
+          loadComponent:() => import('./user-data/user-data.component').then(com => com.UserDataComponent)
+     }
 ];
